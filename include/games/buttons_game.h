184ebf8c81102535b66f3706ddce06cd4fb10f8f
Oscar Sandoval <oscar@daoai.ca>
2023-10-14 14:19:42 -0700

    Function implementation

--- include/games/buttons_game.h
+++ include/games/buttons_game.h
@@ -6,8 +6,7 @@
 namespace VTech {
 
 struct GameInterface {
-	virtual void start () = 0;
-	virtual void end() = 0;
+	virtual void run () = 0;
 };
 
 enum class LedState {
@@ -17,21 +16,57 @@ enum class LedState {
 	ORANGE
 };
 
+class PasswordGenerator {
+public:
+	explicit PasswordGenerator(const std::vector<char>& characters) : characters_(characters)
+	{
+		srand(time(0));
+	}
+
+	std::string generate() const {
+		std::string password;
+		for (int i = 0; i < characters_.size(); ++i)
+			password += characters_.at(rand() % characters_.size());
+
+		return password;
+	}
+protected:
+	const std::vector<char> characters_;
+};
+
+class ButtonsGame;
+struct ButtonsGamePrinter {
+	static std::string print(const ButtonsGame& game);
+};
+
+
 class ButtonsGame: public GameInterface {
 public:
+	using LEDstates = std::vector<LedState>;
+
 	explicit ButtonsGame(const std::string& secret_combination="");
+	virtual ~ButtonsGame();
 
-	void start() override;
+	void run() override;
 
-	void end() override;
+	void onButtonPress(char btn);
 
-	std::vector<LedState> getLights();
-
-	void onButtonPress(char letter);
+	friend std::ostream& operator<<(std::ostream& os, const ButtonsGame& game);
 
 protected:
+	// TODO: use impl idiom to hide implementatin details
+	void generatePass();
+	LEDstates getLights();
+	void setLights(const LEDstates& states);
+	void inputValidation(char input);
+	bool passValidation();
+
 	std::string combination_;
 	std::string secret_combination_;
+	int curr_idx_ = 0;
+	LEDstates led_states_;
+	std::vector<char> valid_chars_;
+	std::unique_ptr<PasswordGenerator> password_generator_;
 };
 
 
--- src/buttons_game.cpp
+++ src/buttons_game.cpp
@@ -1,11 +1,93 @@
+#include <iostream>
 #include "games/buttons_game.h"
+#include "errors.h"
 
-VTech::ButtonsGame::ButtonsGame() {}
+VTech::ButtonsGame::ButtonsGame(const std::string &secret_combination)
+	: valid_chars_ (std::vector<char>{'A', 'B', 'C'})
+{
+	setLights({LedState::OFF, LedState::OFF, LedState::OFF});
+}
 
-void VTech::ButtonsGame::start() {
+void VTech::ButtonsGame::run() {
+	while (true) {
+
+		generatePass();
+
+		// Print game status here
+
+		while (!passValidation()) {
+
+			std::cout << "Press one of the Following letters on your keyboard A B C : ";
+			char btn_input;
+			std::cin >> btn_input;
+			inputValidation(btn_input);
+
+			onButtonPress(btn_input);
+
+			// Print game status here
+		}
+
+		std::cout << "Congrats! You've found the secret code.\n";
+		std::cout << "Starting new game...\n";
+	}
+}
+
+void VTech::ButtonsGame::onButtonPress(char btn) {
+	auto checkGuess = [this](char usr_guess, int pos) {
+		if (usr_guess == secret_combination_.at(pos))
+			return LedState::GREEN;
+
+		// Check if letter is part of the password
+		if (secret_combination_.find(usr_guess) != std::string::npos)
+			return LedState::ORANGE;
+
+		return LedState::RED;
+	};
+
+//	inputValidation(btn);
+	LEDstates prev_state = getLights();
+
+	// Shift led colors
+	LEDstates new_state = {
+			prev_state.at(1),
+			prev_state.at(2),
+			checkGuess(btn, curr_idx_)
+	};
+
+	setLights(new_state);
+}
+
+void VTech::ButtonsGame::generatePass() {
+	password_generator_ = std::make_unique<PasswordGenerator>(valid_chars_);
+	secret_combination_ = password_generator_->generate();
+}
+
+VTech::ButtonsGame::LEDstates VTech::ButtonsGame::getLights() {
+	return led_states_;
+}
+
+void VTech::ButtonsGame::setLights(const VTech::ButtonsGame::LEDstates &states) {
+	led_states_ = states;
+}
+
+void VTech::ButtonsGame::inputValidation(char input) {
+	bool found = std::find(valid_chars_.begin(), valid_chars_.end(), input) != valid_chars_.end();
+	if (!found)
+		throw GameErrorInput("The pressed button isn't valid");
+}
+
+bool VTech::ButtonsGame::passValidation() {
+	return secret_combination_ == combination_;
+}
+
+std::ostream &VTech::operator<<(std::ostream &os, const VTech::ButtonsGame &game) {
+	os << "History: " << game.combination_ << "\n";
+//	os << "LEDs: " << game.led_states_[0] << "\n";
+	return os;
+}
+
+VTech::ButtonsGame::~ButtonsGame() {
 
 }
 
-void VTech::ButtonsGame::end() {
 
-}
