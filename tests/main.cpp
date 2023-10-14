354159135f56844655d880d62a58a0439726600d
Oscar Sandoval <oscar@daoai.ca>
2023-10-14 00:57:49 -0700

    Enable gtests

---
+++ tests/main.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+TEST(VTechGame, init){
+	ASSERT_TRUE(false);
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
